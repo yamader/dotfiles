86e0ed3b638df488640e880f274d5ae9d4967089
YamaD <me@dyama.net>
2023-02-11 10:33:54 +0900

    fishとか

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,50 +1,64 @@
-## ENV
+################################################################
+##  VARS
+################################################################
 
-source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+set -x EDITOR vim
 
+## lang
 #set -x CC clang
 #set -x CXX clang++
-
 set RDMDFLAGS \
   -preview=shortenedMethods
 
-set -x EIX_LIMIT 0
-set -x HOMEBREW_NO_AUTO_UPDATE 1
+## sdk
+set -x DOTNET_CLI_TELEMETRY_OPTOUT true
+set -x NEXT_TELEMETRY_DISABLED 1
 
+## tools
+set -x GPG_TTY (tty)
+set -x EIX_LIMIT 0
+
+################################################################
+##  ENV
+################################################################
+
+## lang
 if type -q java-config
   set -x JAVA_HOME (java-config -O)
 end
-if test -d /opt/homebrew
-  set BREW_PREF (/opt/homebrew/bin/brew --prefix)
-end
 set -x GOPATH ~/.go
-set -x PNPM_HOME ~/.node/pnpm
-set -x DENO_INSTALL ~/.deno
+set -x WASMER_DIR ~/.wasmer
 set -x BUN_INSTALL ~/.bun
-set -x FLYCTL_INSTALL ~/.fly
+set -x DENO_INSTALL ~/.deno
+
+## sdk
 set -x ANDROID_HOME ~/SDK/Android
 set -x NDK_HOME /opt/android-ndk
 
-set -x DOTNET_CLI_TELEMETRY_OPTOUT true
+## pm
+set -x PNPM_HOME ~/.node/pnpm
 
-## PATH
-fish_add_path -P \
-	$BREW_PREF/bin \
-	$BREW_PREF/sbin \
-	$BREW_PREF/opt/coreutils/libexec/gnubin
+## tools
+set -x FLYCTL_INSTALL ~/.fly
+
+################################################################
+##  PATH
+################################################################
+
+## ld
 set -x LD_LIBRARY_PATH \
 	~/SDK/neko \
 	/opt/dmd/linux/lib64 \
 	$LD_LIBRARY_PATH
 
-### LANG
+## lang
 fish_add_path -P \
 	~/repos/lang/v \
 	/opt/dmd/linux/bin64 \
 	$BUN_INSTALL/bin \
 	$JAVA_HOME/bin
 
-### SDK
+## sdk
 fish_add_path -P \
 	~/SDK/FPGA/intel_lite_211/quartus/bin \
 	~/SDK/flutter/bin \
@@ -58,7 +72,7 @@ fish_add_path -P \
 	/opt/android-ndk
 #	~/SDK/depot_tools
 
-### PM
+## pm
 fish_add_path -P \
 	~/.cargo/bin \
 	$GOPATH/bin \
@@ -68,7 +82,22 @@ fish_add_path -P \
 	~/.opam/default/bin \
 	~/bin # gem
 
-### LOCAL
+## local
 fish_add_path -P \
 	~/.bin \
 	~/.local/bin
+
+################################################################
+##  macOS
+################################################################
+
+set -x HOMEBREW_NO_AUTO_UPDATE 1
+
+if test -d /opt/homebrew
+  set BREW_PREF (/opt/homebrew/bin/brew --prefix)
+end
+
+fish_add_path -P \
+	$BREW_PREF/bin \
+	$BREW_PREF/sbin \
+	$BREW_PREF/opt/coreutils/libexec/gnubin
--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,13 @@
-set -x EDITOR vim
-
-set -x GPG_TTY (tty)
-
 #ufetch
 
-# gcc
+################################################################
+##  DEV
+################################################################
+
 if test -e ~/.gcc
   eval (gcc-config -E (cat ~/.gcc))
 end
+
+source $WASMER_DIR/wasmer.sh > /dev/null 2>/dev/null; or true
+
+source ~/.opam/opam-init/init.fish > /dev/null 2>/dev/null; or true
