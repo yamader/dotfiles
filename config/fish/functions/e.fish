a2efaea437e9c4f43fd55c3ea2c3bf4180f85035
YamaD <me@dyama.net>
2023-12-11 20:23:02 +0900

    fish

--- config/fish/functions/e.fish
+++ config/fish/functions/e.fish
@@ -1,4 +1,13 @@
 function e
-  zathura $argv &
+  set -l cmd thunar
+  for i in $argv
+    switch $i
+      case "*.pdf"; set cmd zathura
+      case "*.png" "*.jpg";
+        img2sixel $argv
+        return
+    end
+  end
+  $cmd $argv &
   disown %1
 end
