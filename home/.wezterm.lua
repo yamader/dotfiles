117af9684bd65cb0775bb87f5700d4e22c7c91fe
YamaD <me@dyama.net>
2024-04-09 18:38:59 +0900

    update wezterm

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -1,4 +1,5 @@
 local wt = require "wezterm"
+local act = wt.action
 
 return {
   color_scheme = "Apple System Colors",
@@ -8,6 +9,33 @@ return {
   font = wt.font "FirgeNerd Console",
   font_size = 9,
   hide_tab_bar_if_only_one_tab = true,
+  mouse_bindings = {
+    {
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = act.OpenLinkAtMouseCursor,
+    },
+    {
+      event = { Down = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = act.SelectTextAtMouseCursor "Block",
+    },
+    {
+      event = { Drag = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = act.ExtendSelectionToMouseCursor "Block",
+    },
+    {
+      event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+      mods = "CTRL",
+      action = act.IncreaseFontSize,
+    },
+    {
+      event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+      mods = "CTRL",
+      action = act.DecreaseFontSize,
+    },
+  },
   window_background_opacity = 0.95,
   window_padding = {
     left = 2,
