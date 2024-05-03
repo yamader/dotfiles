cff1ae74cd7d415ba74d853c9485a598e5c84317
YamaD <me@dyama.net>
2024-05-03 09:42:08 +0900

    a

--- config/fish/conf.d/abbr.fish
+++ config/fish/conf.d/abbr.fish
@@ -1,7 +1,10 @@
-abbr -a rep --set-cursor cd ~/Docs/report/%
-abbr -a blog --set-cursor cd ~/Docs/blog/src/content/blog/%
+abbr -a blog --set-cursor ~/Docs/blog/src/content/blog/%
+abbr -a rep --set-cursor ~/Docs/report/%
 
 abbr -a dl --set-cursor ~/DL/%
 abbr -a dv --set-cursor ~/dev/%
 abbr -a rp --set-cursor ~/repos/%
 abbr -a svc --set-cursor ~/service/%
+
+abbr -a m doas emerge
+abbr -a m1 doas emerge -1 -j1
