1b340b8710beb6ddeae8a52bf5a98eaca95b2e7d
YamaD <me@dyama.net>
2025-11-10 13:43:35 +0900

    ensure markdown_inline

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   config = function()
     require("nvim-treesitter.configs").setup {
+      ensure_installed = { "markdown_inline" },
       auto_install = true,
       highlight = {
         enable = true,
