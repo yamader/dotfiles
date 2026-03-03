2ca4f1a28927ce4b651fc0be06a29855f3451db2
YamaD <me@dyama.net>
2026-03-03 11:06:02 +0900

    update firefox user.js

--- other/user.js
+++ other/user.js
@@ -1,1 +1,5 @@
+user_pref("browser.cache.disk.parent_directory", "/run/user/1000/firefox");
+user_pref("browser.compactmode.show", true);
+user_pref("browser.tabs.insertAfterCurrent", true);
+user_pref("browser.tabs.insertAfterCurrentExceptPinned", true);
 user_pref("browser.urlbar.trimURLs", false);
