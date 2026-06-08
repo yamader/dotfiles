4466ce1df5730d844ec8f43e4830295e1742b81c
YamaD <me@dyama.net>
2026-06-08 09:09:36 +0900

    fix firefox hwdec

--- other/user.js
+++ other/user.js
@@ -2,6 +2,7 @@ user_pref("browser.cache.disk.parent_directory", "/run/user/1000/firefox");
 user_pref("browser.compactmode.show", true);
 user_pref("browser.tabs.insertAfterCurrentExceptPinned", true);
 user_pref("browser.urlbar.trimURLs", false);
+user_pref("media.hardware-video-decoding.force-enabled", true); // FEATURE_FAILURE_VIDEO_DECODING_TEST_FAILED
 
 // revert betterfox
 user_pref("browser.cache.disk.enable", true);
